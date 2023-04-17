665bcdcb8eb98832a37dada5706729e8867a3906
Deviacia <ivan.bpemr@gmail.com>
2023-04-17 15:34:14 +0300

    Base layers

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,56 @@
-import React, { useState } from "react";
+import React from "react";
 import { useFinance } from "./hooks/useFinance";
 
 const App = () => {
-  const { wallet, categories, addMoney, addCategory } = useFinance();
-  const [balance, setBalance] = useState(0);
-  const [cat, setCat] = useState("");
+  const { budget, categories, addMoney, addCategory } = useFinance();
 
-  const balanceHandler = (e) => {
-    e.preventDefault();
-    addMoney(balance);
+  const budgetHandler = (e) => {
+    const money = Number(e.target.value);
+    addMoney(money);
   };
 
   const categoryHandler = (e) => {
     e.preventDefault();
 
-    addCategory(cat);
+    const category = e.target[0].value;
+    const sum = e.target[1].value;
+    addCategory(category, sum);
   };
 
   return (
     <div className="container">
       <div>
         <div>
-          <h1>1 step</h1>
-          <span>{wallet}</span>
-          <form onSubmit={(e) => balanceHandler(e)}>
-            <input
-              type="number"
-              value={balance}
-              onChange={(e) => setBalance(e.target.value)}
-            />
-            <button>Add money</button>
-          </form>
+          <h1>1 step - Set budget</h1>
+          <div>
+            <span>{budget}</span>
+          </div>
+          <input onChange={(e) => budgetHandler(e)} type="number" />
         </div>
 
         <div>
-          <h1>2 step</h1>
+          <h1>2 step - Set categories</h1>
           <form onSubmit={(e) => categoryHandler(e)}>
-            <input
-              type="text"
-              value={cat}
-              onChange={(e) => setCat(e.target.value)}
-            />
+            <input type="text" />
+            <input type="number" />
             <button>Add category</button>
           </form>
+
+          <div>
+            {categories.map((category) => (
+              <li key={category.id}>{`${category.name}: ${category.sum}`}</li>
+            ))}
+          </div>
+        </div>
+        {/* Welcome END */}
+
+        <div>
+          <h1>3 step - Add expenses</h1>
+          <div>
+            <span>{budget}</span>
+            <div>Radial chart</div>
+          </div>
+          <input onChange={(e) => budgetHandler(e)} type="number" />
         </div>
       </div>
     </div>
---
+++ src/components/RadialChart.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const RadialChart = () => {
+  const [chart, setChart] = useState({});
+
+  return <div />;
+};
+
+export default RadialChart;
